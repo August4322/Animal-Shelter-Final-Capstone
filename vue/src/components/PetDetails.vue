1741ac3a689ee0f9bf4ed134adb04b03fbfdf4c7
Ruina Kang <ruinakang6@gmail.com>
2022-08-09 14:15:36 -0400

    afternoon vue

--- vue/src/components/PetDetails.vue
+++ vue/src/components/PetDetails.vue
@@ -2,7 +2,10 @@
 <div>
 <div v-for="pet in this.$store.state.pets" v-bind:key="pet.id" class="petCard">
     <div v-if="pet.available">
+     <div id="frame">
     <img v-bind:src="pet.image" alt="picture of animal" class="petImg">
+     </div>
+     <div class="petDetails" >
     <h2 class="petName">{{pet.name}}</h2>
     <h3>{{pet.tagline}}</h3>
     <ul class="details">
@@ -10,6 +13,7 @@
         <li>{{pet.age}}</li>
         <li>{{pet.fixed ? "I am fixed" : "I am not fixed"}} </li>
     </ul>
+    </div>
     </div>
 
      
@@ -49,17 +53,45 @@ export default {
 </script>
 
 <style scoped>
+.petName {
+font-size: 1.5rem;
+}
+div h3 {
+font-size: 1rem;
+}
+
+
  .petImg {
-     width: 200px;
-     height: 
+     max-width: 100%;
+     height: auto;
+     object-fit: contain;
+     display: block;
+     margin-left: auto;
+     margin-right: auto;
+     border-radius: 50%;
+     
+
  }
+ #frame {
+    max-height: 200px;
+    max-width: 200px;
+}
  .petCard {
-     border: solid black 1px;
+     /* border: solid black 1px;
      border-radius: 10px;
      width: 300px;
      height: 500px;
      margin: 15px;
-     padding: 10px;
+     padding: 10px; */
+     border: 2px solid black;
+    border-radius: 10px;
+    width: 250px;
+    height: 550px;
+    margin: 20px;
 
+ }
+ .petDetails{
+
+    padding-top: 78px;
  }
 </style>
