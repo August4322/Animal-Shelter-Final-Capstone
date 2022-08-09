6b54ef7a80d737ad2888c7e2049cf00383eb2eb6
Ruina Kang <ruinakang6@gmail.com>
2022-08-09 11:56:39 -0400

    css

--- vue/src/components/PetDetails.vue
+++ vue/src/components/PetDetails.vue
@@ -1,9 +1,17 @@
 <template>
 <div>
 <div v-for="pet in this.$store.state.pets" v-bind:key="pet.id" class="petCard">
+    <div v-if="pet.available">
     <img v-bind:src="pet.image" alt="picture of animal" class="petImg">
     <h2 class="petName">{{pet.name}}</h2>
     <h3>{{pet.tagline}}</h3>
+    <ul class="details">
+        <li>{{pet.gender}}</li>
+        <li>{{pet.age}}</li>
+        <li>{{pet.fixed ? "I am fixed" : "I am not fixed"}} </li>
+    </ul>
+    </div>
+
      
  
 </div>
@@ -43,5 +51,15 @@ export default {
 <style scoped>
  .petImg {
      width: 200px;
+     height: 
+ }
+ .petCard {
+     border: solid black 1px;
+     border-radius: 10px;
+     width: 300px;
+     height: 500px;
+     margin: 15px;
+     padding: 10px;
+
  }
 </style>
