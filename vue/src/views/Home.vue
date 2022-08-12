c73fe8665b750ba4fe7d7bb2bcd423c31afdd8c2
Ruina Kang <ruinakang6@gmail.com>
2022-08-12 14:46:51 -0400

    fixed and tested the home button

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -3,7 +3,7 @@
     <h1>Home</h1>
     
     <p>You must be authenticated to see this</p>
-    <router-link :to="{ name: 'pageForVolunteer' }" v-if="$store.user.role==='ROLE_ADMIN'">volunteer menu</router-link>
+    <!-- <router-link :to="{ name: 'pageForVolunteer' }" v-if="$store.user.role==='ROLE_ADMIN'">volunteer menu</router-link> -->
     
   </div>
 </template>
